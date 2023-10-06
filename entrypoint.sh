07e8c1fc38a5ac7f693dae23d674fa1cc36b5093
komekopan2 <yoo.shimizu888@gmail.com>
2023-10-06 22:14:54 +0900

    dbコンテナが3306を開いてからapp立ち上げ

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# DBが起動するのを待つ
+/wait-for-it.sh db:3306 --timeout=30
+
+# python -m api.migrate_db
+# ローカル用自動マイグレーション
+
 python -m api.migrate_cloud_db
+# クラウド用自動マイグレーション
 
 uvicorn api.main:app --host 0.0.0.0 --reload
